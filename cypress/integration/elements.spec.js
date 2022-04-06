b763714c817505ce99588f920e25c1771552e41d
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-04-06 11:19:32 -0300

    Campos de texto

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -11,11 +11,44 @@ describe("Work with basic elementes", () => {
     cy.reload(); // limpa a sujeira de um de um teste para outro
   });
 
-  it('Links', () => {
-      cy.get('a').first().click();
-      cy.get('#resultado').should('have.text', 'Voltou!');
+  it("Links", () => {
+    cy.get("a").first().click();
+    cy.get("#resultado").should("have.text", "Voltou!");
 
-      cy.reload();
-      cy.get('#resultado').should('have.not.text', 'Voltou!');
-  } )
+    cy.reload();
+    cy.get("#resultado").should("have.not.text", "Voltou!");
+  });
+
+  it("TextFields", () => {
+    cy.get("#formNome")
+      .type("Cypress test")
+      .should("have.value", "Cypress test");
+    /*
+      'elementosForm:sugestoes' gera erro pois existem alguns comandos 
+      no jquery selector que vão utilizar ':', então isso pode confundí-lo.
+
+      cy.get("#elementosForm:sugestoes")
+      .type("Cypress test two")
+      .should("have.value", "Cypress test two");
+      */
+
+    // Para corrigir pode inserir '\' antes do ':' ou '\\' , veja:
+
+    cy.get("#elementosForm\\:sugestoes")
+      .type("Cypress test two")
+      .should("have.value", "Cypress test two");
+
+    cy.get(":nth-child(2) > :nth-child(6) > input")
+      .type("Cypress test three{enter}")
+      .should("have.value", "Cypress test three");
+
+    cy.get("[data-cy=dataSobrenome]")
+      .type("Teste12345{backspace}")
+      .should("have.value", "Teste1234");
+
+    cy.get("#elementosForm\\:sugestoes")
+      .clear()// usar quando houver sujeira no campo. Outra opção seria fazer um reload()
+      .type('Erro{selectall}Acerto', {delay:100}) //Faz um delay de 100ms na escrita
+      .should("have.value", "Acerto");
+  });
 });
