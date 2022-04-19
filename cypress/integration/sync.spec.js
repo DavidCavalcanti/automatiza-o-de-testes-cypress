4bafc4cb7950ea5975b8e4aab2b18149b8b9dc09
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-04-19 12:50:49 -0300

    Create sync.spec.js

---
+++ cypress/integration/sync.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress"/>
+
+describe("Esperas...", () => {
+  before(() => {
+    cy.visit("https://wcaquino.me/cypress/componentes.html");
+  });
+
+  beforeEach(() => {
+    cy.reload();
+  });
+
+  it("Deve aguardar elemento estar disponível", () => {
+    cy.get("#novoCampo").should("not.exist");
+    cy.get("#buttonDelay").click();
+    cy.get("#novoCampo").should("not.exist");
+    cy.get("#novoCampo").type("funciona");
+  });
+});
