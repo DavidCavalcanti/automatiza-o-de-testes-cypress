81718395150d2ff75a1d2807a0853f003e89e420
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-04 17:55:11 -0300

    Usar wait e timeout

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -16,12 +16,23 @@ describe("Esperas...", () => {
     cy.get("#novoCampo").type("funciona");
   });
 
-  it.only('Uso do find', () => {
-      cy.get('#buttonList').click();
-      cy.get('#Lista li')
-      .find('span')
-      .should('contain', 'Item 1');
-      cy.get('#Lista li span')      
-      .should('contain', 'Item 2');
-  })
+  it("Uso do find", () => {
+    cy.get("#buttonList").click();
+    cy.get("#Lista li").find("span").should("contain", "Item 1");
+    cy.get("#Lista li span").should("contain", "Item 2");
+  });
+
+  it.only("Uso do timout", () => {
+    //cy.get("#buttonDelay").click();
+    //cy.get('#novoCampo', {timeout: 1000}).should('exist');
+
+    //cy.get("#buttonListDOM").click();
+    //cy.get("#lista li span", {timeout: 10000}).should("contain", "Item 2");
+
+    // Para este caso não precisa de timeout porque o a primeir busca já consumiu 
+    // parte do tempo, sendo suficiente para a segunda assertiva ser validada.
+    cy.get("#buttonListDOM").click();
+    cy.get("#lista li span",).should('have.length', 1);
+    cy.get("#lista li span",).should('have.length', 2);
+  });
 });
