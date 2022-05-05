3e711c9b4e2580800535706f12ed0fb0f00eff53
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-05 11:01:52 -0300

    should vs then

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -22,17 +22,36 @@ describe("Esperas...", () => {
     cy.get("#Lista li span").should("contain", "Item 2");
   });
 
-  it.only("Uso do timout", () => {
+  it("Uso do timout", () => {
     //cy.get("#buttonDelay").click();
     //cy.get('#novoCampo', {timeout: 1000}).should('exist');
 
     //cy.get("#buttonListDOM").click();
     //cy.get("#lista li span", {timeout: 10000}).should("contain", "Item 2");
 
-    // Para este caso não precisa de timeout porque o a primeir busca já consumiu 
+    // Para este caso não precisa de timeout porque o a primeir busca já consumiu
     // parte do tempo, sendo suficiente para a segunda assertiva ser validada.
     cy.get("#buttonListDOM").click();
-    cy.get("#lista li span",).should('have.length', 1);
-    cy.get("#lista li span",).should('have.length', 2);
+    cy.get("#lista li span").should("have.length", 1);
+    cy.get("#lista li span").should("have.length", 2);
+  });
+
+
+  // then aguarda o get ser finalizado para então ser  executado
+  // Should não aguarda, ele já faz a busca ao mesmo tempo que faz a verificação
+  it.only("Should vs Then", () => {
+    cy.get("#buttonListDOM").click();
+    cy.get("#lista li span").then((elemento) => {
+      // Dentro deste escopo não estamos mais trabalhando com elementos do cypress,
+      // então não podemos utilizar o should para fazer assertivas. 'elemento' é um
+      // elemento html que foi captura, teremos que tratar como um jquery. Veja como:
+
+      //should('have.length', 1)
+      expect(elemento).to.have.length(1);  
+      return 2;    
+    })
+    .then(elemento =>{
+      expect(elemento).to.have.length(2);
+    });
   });
 });
