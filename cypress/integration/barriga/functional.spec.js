61ea1de7fb77508789f1d9ee69e87b983e8929d8
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-06 11:52:24 -0300

    Criar conta

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -9,7 +9,12 @@ describe("should test at a functional level", () => {
     cy.get(".toast-message").should("contain", 'Bem vindo');
   });
 
-  it("...", () => {
+  it("Should create an count", () => {
+    cy.get('[data-test=menu-settings]').click();
+    cy.get('[href="/contas"]').click();    
+    cy.get('[data-test=nome]').type('teste');    
+    cy.get('.btn').click();
+    cy.get('.toast-success > .toast-message').should("contain", "Conta inserida com sucesso!")
     
   });
 });
