ce374e574ccc4deaa96a42c784da3b760a317195
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-06 11:34:36 -0300

    Organizar login
    Login sempre será chamada antes de cada teste, por este motivo deve fica no before

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -2,6 +2,14 @@
 
 describe("should test at a functional level", () => {
   before(() => {
-    cy.visit("https://barrigareact.waquino.me/");
+    cy.visit("https://barrigareact.wcaquino.me/");
+    cy.get("[data-test=email]").type("a@a");
+    cy.get("[data-test=passwd]").type("a");
+    cy.get(".btn").click();
+    cy.get(".toast-message").should("contain", 'Bem vindo');
+  });
+
+  it("...", () => {
+    
   });
 });
