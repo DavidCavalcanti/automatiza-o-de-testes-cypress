7d01c5ca3a066b108d7413543278703a3fc8f4e1
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-08 14:20:54 -0300

    alterar conta criada

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -6,15 +6,28 @@ describe("should test at a functional level", () => {
     cy.get("[data-test=email]").type("a@a");
     cy.get("[data-test=passwd]").type("a");
     cy.get(".btn").click();
-    cy.get(".toast-message").should("contain", 'Bem vindo');
+    cy.get(".toast-message").should("contain", "Bem vindo");
   });
 
   it("Should create an count", () => {
-    cy.get('[data-test=menu-settings]').click();
-    cy.get('[href="/contas"]').click();    
-    cy.get('[data-test=nome]').type('teste');    
-    cy.get('.btn').click();
-    cy.get('.toast-success > .toast-message').should("contain", "Conta inserida com sucesso!")
-    
+    cy.get("[data-test=menu-settings]").click();
+    cy.get('[href="/contas"]').click();
+    cy.get("[data-test=nome]").type("Conta de Teste");
+    cy.get(".btn").click();
+    cy.get(".toast-success > .toast-message").should(
+      "contain",
+      "Conta inserida com sucesso!"
+    );
+  });
+
+  it("Should update an account", () => {
+    cy.get("[data-test=menu-settings]").click();
+    cy.get('[href="/contas"]').click();
+    cy.xpath(
+      "//table//td[contains(.,'Conta de Teste')]/..//i[@class='far fa-edit']"
+    ).click();
+    cy.get("[data-test=nome]").clear().type("Conta alterada");
+    cy.get(".btn").click();
+    cy.get(".toast-message").should("contain", "Conta atualizada com sucesso!");
   });
 });
