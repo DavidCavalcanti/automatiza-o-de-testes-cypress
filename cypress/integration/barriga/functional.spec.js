1436ae0584fa3a0a2171ac836693d31d38a361a1
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-09 14:42:03 -0300

    Importar e alterar comandos

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress"/>
 import { LOCATORS as loc } from "../../support/locators";
+import "../../support/commandsConta";
 
 describe("should test at a functional level", () => {
   before(() => {
@@ -8,16 +9,13 @@ describe("should test at a functional level", () => {
   });
 
   it("Should create an count", () => {
-    cy.get(loc.MENU.SETTINGS).click();
-    cy.get(loc.MENU.CONTAS).click();
-    cy.get(loc.CONTAS.NOME).type("Conta de Teste");
-    cy.get(loc.CONTAS.BTN_SALVAR).click();
+    cy.acessarMenuConta();
+    cy.inserirConta("Conta de Teste");
     cy.get(loc.MESSAGE).should("contain", "Conta inserida com sucesso!");
   });
 
   it("Should update an account", () => {
-    cy.get(loc.MENU.SETTINGS).click();
-    cy.get(loc.MENU.CONTAS).click();
+    cy.acessarMenuConta();
     cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click();
     cy.get(loc.CONTAS.NOME).clear().type("Conta alterada");
     cy.get(loc.CONTAS.BTN_SALVAR).click();
