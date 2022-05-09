8bf8e70dc6585a8b7c50b764be789334707839fb
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-09 14:58:08 -0300

    Inserir conta repetida

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -17,8 +17,14 @@ describe("should test at a functional level", () => {
   it("Should update an account", () => {
     cy.acessarMenuConta();
     cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click();
-    cy.get(loc.CONTAS.NOME).clear().type("Conta alterada");
+   cy.inserirConta("Conta alterada");
     cy.get(loc.CONTAS.BTN_SALVAR).click();
     cy.get(loc.MESSAGE).should("contain", "Conta atualizada com sucesso!");
+  });
+
+  it("Should not create an account with same name", () => {
+    cy.acessarMenuConta();
+    cy.inserirConta("Conta alterada");
+    cy.get(loc.MESSAGE).should('contain','code 400');
   });
 });
