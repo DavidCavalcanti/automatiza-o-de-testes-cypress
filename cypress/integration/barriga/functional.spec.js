c75926c0650cf8279f2891d0c86aca12a6cc9411
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-09 17:04:03 -0300

    Testar  transação

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress"/>
 import { LOCATORS as loc } from "../../support/locators";
 import "../../support/commandsConta";
+import "../../support/commandMovimentacao";
 
 describe("should test at a functional level", () => {
   before(() => {
@@ -17,7 +18,7 @@ describe("should test at a functional level", () => {
   it("Should update an account", () => {
     cy.acessarMenuConta();
     cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click();
-   cy.inserirConta("Conta alterada");
+    cy.inserirConta("Conta alterada");
     cy.get(loc.CONTAS.BTN_SALVAR).click();
     cy.get(loc.MESSAGE).should("contain", "Conta atualizada com sucesso!");
   });
@@ -25,6 +26,17 @@ describe("should test at a functional level", () => {
   it("Should not create an account with same name", () => {
     cy.acessarMenuConta();
     cy.inserirConta("Conta alterada");
-    cy.get(loc.MESSAGE).should('contain','code 400');
+    cy.get(loc.MESSAGE).should("contain", "code 400");
+  });
+
+  it.only("Should create a transaction", () => {
+    cy.get(loc.MENU.MOVIMENTACAO).click();
+    cy.inserirDescricao("Descrição");
+    cy.inserirValor(100);
+    cy.inserirInteressado("Interesse");
+    cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click();
+    cy.get(loc.MESSAGE).should("contain", "Movimentação inserida");
+    //Este xpath está checando o nome o valor associado a ele
+    cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should("exist");
   });
 });
