cd6e07ec87817a3786da35795ec56d28685f3296
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-31 12:04:40 -0300

    Login - requisição API

--- cypress/integration/barriga/backend.spec copy.js
+++ cypress/integration/barriga/backend.spec copy.js
@@ -3,29 +3,31 @@
 import "../../support/commandMovimentacao";
 
 describe("should test at a functional level", () => {
-  before(() => {
-    
-  });
+  before(() => {});
 
   beforeEach(() => {
     //cy.resetApp();
   });
 
   it("Should create an count", () => {
+    cy.request({ // método para fazer requisições à API
+      method: "POST",
+      url: "https://barrigarest.wcaquino.me/signin",
+      body: {
+        email: "a@a",
+        redirecionar: false,
+        senha: "a"
+      },
+    }).its('body.token').should("not.be.empty");
   });
 
-  it("Should update an account", () => {  
-  });
+  it("Should update an account", () => {});
 
-  it("Should not create an account with same name", () => {
-  });
+  it("Should not create an account with same name", () => {});
 
-  it("Should create a transaction", () => {        
-  });
+  it("Should create a transaction", () => {});
 
-  it.only("Should get balance", () => {
-  });
+  it("Should get balance", () => {});
 
-  it("Should remove a transaction", () => {
-  });
+  it("Should remove a transaction", () => {});
 });
