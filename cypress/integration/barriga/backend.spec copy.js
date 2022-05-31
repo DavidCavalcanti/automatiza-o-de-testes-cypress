0ab97c5088aef23540fa65ad08c785888c99ae91
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-05-31 13:25:47 -0300

    Assertiva - Inserir Conta

--- cypress/integration/barriga/backend.spec copy.js
+++ cypress/integration/barriga/backend.spec copy.js
@@ -26,12 +26,18 @@ describe("should test at a functional level", () => {
         cy.request({
           url: "https://barrigarest.wcaquino.me/contas",
           method: "POST",
-          headers: {Authorization: `JWT ${token}`},
+          headers: { Authorization: `JWT ${token}` },
           body: {
             nome: "Conta via api rest",
           },
-        }).then((res) => console.log(res));
+        }).as("response");
       });
+
+    cy.get("@response").then((res) => {
+      expect(res.status).to.be.equal(201);
+      expect(res.body).to.have.property("id");
+      expect(res.body).to.have.property("nome", "Conta via api rest");
+    });
   });
 
   it("Should update an account", () => {});
